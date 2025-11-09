99e6a44242aab8eb0d704144523874baa40751af
Racknet21 <samruddhims04@gmail.com>
2025-11-09 19:40:48 +0530

    Add files via upload

---
+++ exp8.3.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+class MyClass{
+    private:
+    int value;
+    public:
+    MyClass(int val)
+    :value(val)
+    {
+
+    }
+    bool operator==(const MyClass& other)const
+    {
+        return value == other.value;
+    }
+    bool operator!=(const MyClass& other)const
+    {
+        return !(*this == other);
+    }
+    bool operator<(const MyClass& other)const
+    {
+        return value<other.value;
+    }
+    bool operator>(const MyClass& other)const
+    {
+        return value>other.value;
+    }
+    bool operator<=(const MyClass& other)const{
+        return !(*this > other);
+    }
+    bool operator>=(const MyClass& other)const{
+        return !(*this < other);
+    }
+
+};
+int main()
+{
+    cout<<"Name: Samruddhi M Suryawanshi"<<endl;
+    cout<<"div: B"<<endl;
+    cout<<"Roll No: 42"<<endl;
+    
+    MyClass obj1(20);
+    MyClass obj2(20);
+    if (obj1 == obj2){
+        cout<<"obj1 is equal to obj2"<<endl;
+    }
+    else {
+        cout<<"obj1 is not equal to obj2"<<endl;
+    }
+    if(obj1 < obj2){
+        cout<<"obj1 is less than obj2"<<endl;
+    }
+    else {
+        cout<<"obj1 is not less than obj2"<<endl;
+    }
+    if(obj1 != obj2){
+        cout<<"obj1 is not equal to obj2"<<endl;
+    }
+    else {
+        cout<<"obj1 is equal to obj2"<<endl;
+    }
+    if(obj1 > obj2){
+        cout<<"obj1 is greater than obj2"<<endl;
+    }
+    else {
+        cout<<"obj1 is not greater than obj2"<<endl;
+    }
+    if(obj1 <= obj2){
+        cout<<"obj1 is less than or equal to obj2"<<endl;
+    }
+    else{
+        cout<<"obj1 is not less than or equal to obj2"<<endl;
+    }
+    if(obj1 >= obj2){
+        cout<<"obj1 is greater than or equal to obj2"<<endl;
+    }
+    else{
+        cout<<"obj1 is not greater than or equal to obj2"<<endl;
+    }
+    return 0;
+}
